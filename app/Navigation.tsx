f42782b92a9f606be96a79ab6b232503deb27536
Shaun Sylvain David <shaunsylvaindavid@gmail.com>
2025-01-28 01:45:39 +0530

    Updated navigation

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -4,7 +4,10 @@ import HomeScreen from "./_components/login/HomeScreen";
 import LoginScreen from "./_components/login/LoginScreen";
 import RegisterScreen from "./_components/login/RegisterScreen";
 import Dashboard from "./_components/dashboard/Dashboard";
-
+import FirstAid from "./_components/aid/FirstAid";
+import ProfilePage from "./_components/profile/ProfilePage";
+import EditInfo from "./_components/profile/EditInfo";
+import FirstAidList from "./_components/aid/FirstAidList";
 const Stack = createStackNavigator();
 
 const EntryNavigation = () => {
@@ -19,6 +22,10 @@ const EntryNavigation = () => {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Screen name="ProfilePage" component={ProfilePage} />
+      <Stack.Screen name="FirstAid" component={FirstAid} />
+      <Stack.Screen name="FirstAidList" component={FirstAidList} />
+      <Stack.Screen name="EditInfo" component={EditInfo} />
     </Stack.Navigator>
   );
 };
