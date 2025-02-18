f3f10c4948e8630ea17b2dff42b453f27db7ef24
Shaun Sylvain David <shaunsylvaindavid@gmail.com>
2025-02-18 12:06:14 +0530

    Added Logout

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./_components/login/HomeScreen";
 import LoginScreen from "./_components/login/LoginScreen";
@@ -9,10 +9,18 @@ import ProfilePage from "./_components/profile/ProfilePage";
 import EditInfo from "./_components/profile/EditInfo";
 import FirstAidList from "./_components/aid/FirstAidList";
 import MapScreen from "./_components/location/MapScreen";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { FIREBASE_AUTH } from "@/FirebaseConfig";
 
 const Stack = createStackNavigator();
 
 const EntryNavigation = () => {
+  // const [user, setUser] = useState<User | null>(null);
+  // useEffect(() => {
+  //   onAuthStateChanged(FIREBASE_AUTH, (user) => {
+  //     setUser(user);
+  //   });
+  // }, []);
   return (
     <Stack.Navigator
       initialRouteName="Home"
@@ -20,8 +28,8 @@ const EntryNavigation = () => {
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Dashboard" component={Dashboard} />
       <Stack.Screen name="ProfilePage" component={ProfilePage} />
