7dabbfbc462fa0ccd7904b794357bf3b0e2c9412
Shaun Sylvain David <shaunsylvaindavid@gmail.com>
2025-03-11 14:48:38 +0530

    Removed Aid

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -4,10 +4,8 @@ import HomeScreen from "./_components/login/HomeScreen";
 import LoginScreen from "./_components/login/LoginScreen";
 import RegisterScreen from "./_components/login/RegisterScreen";
 import Dashboard from "./_components/dashboard/Dashboard";
-import FirstAid from "./_components/aid/FirstAid";
 import ProfilePage from "./_components/profile/ProfilePage";
 import EditInfo from "./_components/profile/EditInfo";
-import FirstAidList from "./_components/aid/FirstAidList";
 import MapScreen from "./_components/location/MapScreen";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { FIREBASE_AUTH } from "@/FirebaseConfig";
@@ -33,8 +31,6 @@ const EntryNavigation = () => {
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Dashboard" component={Dashboard} />
       <Stack.Screen name="ProfilePage" component={ProfilePage} />
-      <Stack.Screen name="FirstAid" component={FirstAid} />
-      <Stack.Screen name="FirstAidList" component={FirstAidList} />
       <Stack.Screen name="EditInfo" component={EditInfo} />
       <Stack.Screen name="Map" component={MapScreen} />
     </Stack.Navigator>
